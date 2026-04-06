07a614de9bb0b9049a67fca3c8e15a3a20a5f2af
Uttam <uttamkumarjena507@gmail.com>
2026-04-06 09:28:19 +0530

    User Service interface and package updated  - microservices e-commerce system

---
+++ payment-service/src/test/java/com/ecommerce/Payment/Service/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Payment.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
